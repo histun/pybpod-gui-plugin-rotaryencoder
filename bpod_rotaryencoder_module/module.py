ad143b0558d92f073e156ae8fd4f44ff703ea12e
Ricardo Ribeiro <ricardojvr@gmail.com>
2017-09-15 10:42:04 +0100

    .

--- bpod_rotaryencoder_module/module.py
+++ bpod_rotaryencoder_module/module.py
@@ -2,31 +2,49 @@ from pybpodapi.bpod.com.arcom import ArCOM, ArduinoTypes
 
 class RotaryEncoderModule(object):
 
-	def __init__(self, serialport):
+	COM_HANDSHAKE 		= 'C'
+	COM_TOGGLEEVTTRANSM = 'V'
+	COM_TOGGLESTREAM 	= 'S'
+	COM_STARTLOGGING 	= 'L'
+	COM_STOPLOGGING 	= 'F'
+	COM_GETLOGDATA 		= 'R'
+	COM_GETCURRENTPOS  	= 'Q'
+	COM_SETZEROPOS 		= 'Z'
+	COM_SETPOS 			= 'P'
+	COM_ENABLETHRESHOLDS = ';'
+	COM_SETPREFIX	 	= 'I'
+	COM_SETTHRESHOLDS 	= 'T'
+	COM_SETWRAPPOINT 	= 'W'
+
+
+	def __init__(self, serialport=None):
+		if serialport: self.open(serialport)
+
+	def open(self, serialport):
 		self.arcom = ArCOM().open(serialport, 115200)
-
-		self.arcom.write_char('C')
-		response = self.arcom.read_uint8()
-
-		if response != 217:
+		self.arcom.write_char(self.COM_HANDSHAKE)
+		if self.arcom.read_uint8() != 217:
 			raise Exception('Could not connect =( ')
+
 
 	def close(self): self.arcom.close()
 
-	def __pos_2_degrees(self, pos):
-		return round(((float(pos)/512.0)*180.0)*10.0)/10.0;
-
-	def __degrees_2_pos(self, degrees):
-		return int(round( (float(degrees)/180.0)*512.0, 0) );
+	def __pos_2_degrees(self, pos): 	return round(((float(pos)/512.0)*180.0)*10.0)/10.0;
+	def __degrees_2_pos(self, degrees): return int(round( (float(degrees)/180.0)*512.0, 0) );
 
 
+	def enable_evt_transmission(self):
+		self.arcom.write_array([ord(self.COM_TOGGLEEVTTRANSM), 1])
+		return self.arcom.read_uint8()==1
 
 
-	def start_stream(self):
-		self.arcom.write_array([ord('S'), 1])
+	def disable_evt_transmission(self):
+		self.arcom.write_array([ord(self.COM_TOGGLEEVTTRANSM), 0])
+		return self.arcom.read_uint8()==1
 
-	def stop_stream(self):
-		self.arcom.write_array([ord('S'), 0])
+
+	def enable_stream(self):  self.arcom.write_array([ord(self.COM_TOGGLESTREAM), 1])
+	def disable_stream(self): self.arcom.write_array([ord(self.COM_TOGGLESTREAM), 0])
 
 	def read_stream(self):
 		data = []
@@ -43,19 +61,16 @@ class RotaryEncoderModule(object):
 				
 				position_degrees = self.__pos_2_degrees(position)
 				
-				data.insert(0, (evt_time, position_degrees))
+				data.append((evt_time, position_degrees))
 			
 		return data
 
 
-	def start_logging(self):
-		self.arcom.write_array([ord('L')])
-
-	def stop_logging(self):
-		self.arcom.write_array([ord('F')])
+	def enable_logging(self): 	self.arcom.write_array([ord(self.COM_STARTLOGGING)])
+	def disable_logging(self): 	self.arcom.write_array([ord(self.COM_STOPLOGGING)])
 
 	def get_logged_data(self):
-		self.arcom.write_array([ord('R')])
+		self.arcom.write_array([ord(self.COM_GETLOGDATA)])
 		msg    = self.arcom.read_bytes_array(4)
 		n_logs = int.from_bytes( b''.join(msg), byteorder='little', signed=False)
 		data = []
@@ -65,34 +80,53 @@ class RotaryEncoderModule(object):
 			position = int.from_bytes( data_in_bytes[i:i+2],   byteorder='little', signed=True)
 			evt_time = float(int.from_bytes( data_in_bytes[i+2:i+6], byteorder='little', signed=False))/1000.0
 			position_degrees = self.__pos_2_degrees(position)				
-			data.insert(0, (evt_time, position_degrees))
+			data.append((evt_time, position_degrees))
 			
 		return data
 
 
 
 	def current_position(self):
-		self.arcom.write_array([ord('Q')])
+		self.arcom.write_array([ord(self.COM_GETCURRENTPOS)])
 		data_in_bytes = b''.join(self.arcom.read_bytes_array(2))
 		ticks = int.from_bytes( data_in_bytes, byteorder='little', signed=True)
 		return self.__pos_2_degrees(ticks)
 		
 	def set_zero_position(self):
-		self.arcom.write_array([ord('Z')])
+		self.arcom.write_array([ord(self.COM_SETZEROPOS)])
+
+	def set_prefix(self, prefix):
+		self.arcom.write_array([ord(self.COM_SETPREFIX), prefix])
+		return self.arcom.read_uint8()==1
+
+	def set_thresholds(self, thresholds):
+		data  = ArduinoTypes.get_uint8_array([ord(self.COM_SETTHRESHOLDS), len(thresholds) ])
+		data += ArduinoTypes.get_uint16_array([ self.__degrees_2_pos(thresh) for thresh in thresholds])
+		self.arcom.write_array(data )
+		return self.arcom.read_uint8()==1
 		
 	def set_position(self, degrees):
-		ticks = self.__degrees_2_pos(degrees)		
-		data = ArduinoTypes.get_uint8_array([ord('P')])+ticks.to_bytes(2, byteorder='little', signed=True)
+		ticks = self.__degrees_2_pos(degrees)
+		data  = ArduinoTypes.get_uint8_array([ord(self.COM_SETPOS)])
+		data += ticks.to_bytes(2, byteorder='little', signed=True)
+		
 		self.arcom.write_array(data)
-		return self.arcom.read_uint8()
+		return self.arcom.read_uint8()==1
+
+	def set_wrappoint(self, wrap_point):
+		ticks = self.__degrees_2_pos(wrap_point)
+		self.arcom.write_array([ord(self.COM_SETWRAPPOINT)]+ ArduinoTypes.get_uint16_array([ticks]) )
+		return self.arcom.read_uint8()==1
 
 	def enable_thresholds(self, thresholds):
 		if len(thresholds)!=8: raise Exception('Thresholds array has to be of length 8')
 		string = ''.join(map(lambda x: str(int(x)), thresholds))
 		bits = int(string, 2)
-		self.arcom.write_array([ord(';'), bits])
+		self.arcom.write_array([ord(self.COM_ENABLETHRESHOLDS), bits])
 
 
+	@property
+	def is_open(self): return self.arcom.is_open
 
 
 
@@ -101,11 +135,15 @@ if __name__=='__main__':
 
 	m = RotaryEncoderModule('/dev/ttyACM1')
 
-	m.start_logging()
-	#m.start_stream()
+	#m.start_logging()
 	
-	
+	#m.stop_logging()
+
 	"""
+	m.enable_stream()
+	
+	
+	
 	count = 0
 	while count<10:
 		data = m.read_stream()
@@ -114,9 +152,10 @@ if __name__=='__main__':
 		print(data)
 		count += 1
 
-	m.stop_stream()
+	m.disable_stream()
 	"""
-	print('set', m.set_position(299))
+	print('set', m.set_position(179))
+	m.set_zero_position()
 
 	m.enable_thresholds([True, False, True, True, False, False, True, True])
 	print(m.current_position())
---
+++ bpod_rotaryencoder_module/module_gui.py
@@ -0,0 +1,144 @@
+import pyforms, sip
+from pyforms import BaseWidget
+from pyforms.Controls import ControlText, ControlCheckBox, ControlNumber, ControlButton
+from pyforms.Controls import ControlMatplotlib
+from bpod_rotaryencoder_module.module import RotaryEncoderModule
+
+from pysettings import conf
+if conf.PYFORMS_USE_QT5:
+	from PyQt5.QtCore import QTimer, QEventLoop
+else:
+	from PyQt4.QtCore import QTimer, QEventLoop
+
+class RotaryEncoderModuleGUI(RotaryEncoderModule, BaseWidget):
+
+	TITLE = 'Rotary encoder module'
+
+	def __init__(self):
+		BaseWidget.__init__(self, self.TITLE)
+		RotaryEncoderModule.__init__(self)
+
+		self._port 			= ControlText('Serial port', '/dev/ttyACM1')
+		self._connect_btn   = ControlButton('Connect', checkable=True)
+
+		self._events 		= ControlCheckBox('Enable events')
+		self._stream 		= ControlCheckBox('Stream data')
+		self._zero_btn 		= ControlButton('Reset position')
+		self._reset_threshs = ControlButton('Reset thresholds')
+		self._thresh_lower 	= ControlNumber('Lower threshold (deg)', 0, -360, 360)
+		self._thresh_upper 	= ControlNumber('Upper threshold (deg)', 0, -360, 360)
+		self._graph 		= ControlMatplotlib('Value')
+		self._clear_btn 	= ControlButton('Clear')
+
+		self.set_margin(10)
+
+		self.formset = [
+			('_port','_connect_btn'),
+			('_events', '_stream','_zero_btn',),
+			('_thresh_lower', '_thresh_upper', '_reset_threshs'),			
+			
+			'_graph',
+			'_clear_btn'
+		]
+
+		self._stream.enabled = False
+		self._events.enabled = False
+		self._zero_btn.enabled = False
+		self._reset_threshs.enabled = False
+		self._thresh_lower.enabled = False
+		self._thresh_upper.enabled = False
+
+		self._connect_btn.value = self.__toggle_connection_evt
+		self._stream.changed_event = self.__stream_changed_evt
+		self._events.changed_event = self.__events_changed_evt
+		self._thresh_upper.changed_event = self.__thresh_evt
+		self._thresh_lower.changed_event = self.__thresh_evt
+		self._reset_threshs.value = self.__reset_thresholds_evt
+		self._zero_btn.value = self.__zero_btn_evt
+		self._graph.on_draw = self.__on_draw_evt
+		self._clear_btn.value = self.__clear_btn_evt
+
+		self.history_x = []
+		self.history_y = []
+
+		self._timer = QTimer()
+		self._timer.timeout.connect(self.__update_graph)
+
+	def __clear_btn_evt(self):
+		self.history_x = []
+		self.history_y = []
+		self._graph.draw()
+
+	def __on_draw_evt(self, figure):
+		axes = figure.add_subplot(111)
+		axes.clear();
+		axes.plot(self.history_x, self.history_y) 
+
+		if len(self.history_x)>=2:
+			x_range = [self.history_x[0],self.history_x[-1]]
+			axes.plot(x_range,[self._thresh_upper.value, self._thresh_upper.value], linestyle='dotted', color='red')
+			axes.plot(x_range,[self._thresh_lower.value, self._thresh_lower.value], linestyle='dotted', color='blue')
+
+		self._graph.repaint()
+		#print(self.history_x, self.history_y)
+
+	def __update_graph(self):
+		for data in self.read_stream():
+			self.history_x.append(data[0])
+			self.history_y.append(data[1])
+
+		self._graph.draw()
+
+	def __zero_btn_evt(self): 
+		self.set_zero_position()
+
+	def __reset_thresholds_evt(self):
+		pass
+
+
+	def __thresh_evt(self):
+		thresholds = [int(self._thresh_lower.value), int(self._thresh_upper.value) ]	
+		self.set_thresholds(thresholds)
+
+
+	def __stream_changed_evt(self):
+		if self._stream.value:
+			self.enable_stream()
+			self._timer.start(1000)
+		else:
+			self.disable_stream()
+			self._timer.stop()
+
+	def __events_changed_evt(self):
+		if self._stream.value:
+			self.enable_evt_transmission()
+		else:
+			self.disable_evt_transmission()
+
+	def __toggle_connection_evt(self):
+		if not self._connect_btn.checked:
+			if hasattr(self, 'arcom'):
+				self.disable_stream()
+				self._timer.stop()
+				self.close()
+			self._connect_btn.label = 'Connect'
+			self._stream.enabled = False
+			self._events.enabled = False
+			self._zero_btn.enabled = False
+			self._reset_threshs.enabled = False
+			self._thresh_lower.enabled = False
+			self._thresh_upper.enabled = False
+		else:
+			self.open(self._port.value)
+			self._connect_btn.label = 'Connected'
+			self._stream.enabled = True
+			self._events.enabled = True
+			self._zero_btn.enabled = True
+			self._reset_threshs.enabled = True
+			self._thresh_lower.enabled = True
+			self._thresh_upper.enabled = True
+
+
+
+if __name__=='__main__':
+	pyforms.start_app( RotaryEncoderModuleGUI, geometry=(0,0,600,500) )
