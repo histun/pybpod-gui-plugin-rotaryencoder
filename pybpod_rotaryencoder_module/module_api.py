f9f69799d20c9e51781cb68cc45cbe13a0cf24ed
Luís Teixeira <micboucinha@gmail.com>
2019-07-25 16:39:15 +0100

    Style update to comply with flake8
    And reordered methods to the same order as the official bpod documentation

--- pybpod_rotaryencoder_module/module_api.py
+++ pybpod_rotaryencoder_module/module_api.py
@@ -1,5 +1,6 @@
 from pybpodapi.com.arcom import ArCOM, ArduinoTypes
 
+
 class RotaryEncoderModule(object):
 
     COM_HANDSHAKE        = 'C'
@@ -16,7 +17,6 @@ class RotaryEncoderModule(object):
     COM_SETTHRESHOLDS    = ord('T')
     COM_SETWRAPPOINT     = ord('W')
 
-
     def __init__(self, serialport=None):
         """
         Constructer of the RotaryEncoderModule object
@@ -24,7 +24,8 @@ class RotaryEncoderModule(object):
 
         :ivar str serialport: PC serial port where the module is connect
         """
-        if serialport: self.open(serialport)
+        if serialport:
+            self.open(serialport)
 
     def open(self, serialport):
         """
@@ -37,32 +38,31 @@ class RotaryEncoderModule(object):
         if self.arcom.read_uint8() != 217:
             raise Exception('Could not connect =( ')
 
-
     def close(self):
         """
         Closes the serial connection to the Rotary Encoder board.
         """
         self.arcom.close()
 
-    def __pos_2_degrees(self, pos):     return round(((float(pos)/512.0)*180.0)*10.0)/10.0;
-    def __degrees_2_pos(self, degrees): return int(round( (float(degrees)/180.0)*512.0, 0) );
+    def __pos_2_degrees(self, pos):
+        return round(((float(pos)/512.0)*180.0)*10.0)/10.0
 
+    def __degrees_2_pos(self, degrees):
+        return int(round((float(degrees)/180.0)*512.0, 0))
 
     def enable_evt_transmission(self):
         """
         Enable the transmission of the events.
         """
         self.arcom.write_array([self.COM_TOGGLEEVTTRANSM, 1])
-        return self.arcom.read_uint8()==1
-
+        return self.arcom.read_uint8() == 1
 
     def disable_evt_transmission(self):
         """
         Disable the transmission of the events.
         """
         self.arcom.write_array([self.COM_TOGGLEEVTTRANSM, 0])
-        return self.arcom.read_uint8()==1
-
+        return self.arcom.read_uint8() == 1
 
     def enable_stream(self):
         """
@@ -83,25 +83,65 @@ class RotaryEncoderModule(object):
         res = []
         available = self.arcom.bytes_available()
 
-        if available>1:
-            msg  = self.arcom.read_bytes_array(available)
-            
-            while len(msg)>=7:
-                if msg[0]==b'P':
-                    position = int.from_bytes( b''.join(msg[1:3]),       byteorder='little', signed=True)
-                    evt_time = float(int.from_bytes( b''.join(msg[3:7]), byteorder='little', signed=False))/1000.0
+        if available > 1:
+            msg = self.arcom.read_bytes_array(available)
+
+            while len(msg) >= 7:
+                if msg[0] == b'P':
+                    position = int.from_bytes(b''.join(msg[1:3]), byteorder='little', signed=True)
+                    evt_time = float(int.from_bytes(b''.join(msg[3:7]), byteorder='little', signed=False))/1000.0
                     position_degrees = self.__pos_2_degrees(position)
-                    res.append(['P',evt_time, position_degrees])
-                elif msg[0]==b'E':
+                    res.append(['P', evt_time, position_degrees])
+                elif msg[0] == b'E':
                     origin = msg[1]
-                    event  = msg[2]
-                    evt_time = float(int.from_bytes( b''.join(msg[3:7]), byteorder='little', signed=False))/1000.0
-                    res.append(['E',evt_time, origin, event])
-                
+                    event = msg[2]
+                    evt_time = float(int.from_bytes(b''.join(msg[3:7]), byteorder='little', signed=False))/1000.0
+                    res.append(['E', evt_time, origin, event])
+
                 msg = msg[7:]
-        
+
         return res
 
+    def current_position(self):
+        """
+        Retrieve the current position.
+        """
+        self.arcom.write_array([self.COM_GETCURRENTPOS])
+        data_in_bytes = b''.join(self.arcom.read_bytes_array(2))
+        ticks = int.from_bytes(data_in_bytes, byteorder='little', signed=True)
+        return self.__pos_2_degrees(ticks)
+
+    def set_zero_position(self):
+        """
+        Set current rotary encoder position to zero.
+        """
+        self.arcom.write_array([self.COM_SETZEROPOS])
+
+    def set_position(self, degrees):
+        """
+        Set the current position in degrees.
+
+        :ivar int degrees: current position in degrees.
+        """
+        ticks = self.__degrees_2_pos(degrees)
+        data = ArduinoTypes.get_uint8_array([self.COM_SETPOS])
+        data += ticks.to_bytes(2, byteorder='little', signed=True)
+
+        self.arcom.write_array(data)
+        return self.arcom.read_uint8() == 1
+
+    def enable_thresholds(self, thresholds):
+        """
+        Enable the thresholds.
+
+        :ivar list(boolean) thresholds: list of 6 booleans indicating which thresholds are active to trigger events.
+        """
+        if len(thresholds) != 8:
+            raise Exception('Thresholds array has to be of length 8')
+
+        string = ''.join(map(lambda x: str(int(x)), thresholds))
+        bits = int(string, 2)
+        self.arcom.write_array([self.COM_ENABLETHRESHOLDS, bits])
 
     def enable_logging(self):
         """
@@ -120,37 +160,20 @@ class RotaryEncoderModule(object):
         Retreave the logged data in the SD Card.
         """
         self.arcom.write_array([self.COM_GETLOGDATA])
-        msg    = self.arcom.read_bytes_array(4)
-        n_logs = int.from_bytes( b''.join(msg), byteorder='little', signed=False)
-        data   = []
+        msg = self.arcom.read_bytes_array(4)
+        n_logs = int.from_bytes(b''.join(msg), byteorder='little', signed=False)
+        data = []
 
         for i in range(0, n_logs):
             msg = self.arcom.read_bytes_array(8)
             data_in_bytes = b''.join(msg)
 
-            position = int.from_bytes( data_in_bytes[:4],       byteorder='little', signed=True)
-            evt_time = float(int.from_bytes( data_in_bytes[4:], byteorder='little', signed=False))/1000.0
+            position = int.from_bytes(data_in_bytes[:4], byteorder='little', signed=True)
+            evt_time = float(int.from_bytes(data_in_bytes[4:], byteorder='little', signed=False))/1000.0
             position_degrees = self.__pos_2_degrees(position)
             data.append((evt_time, position_degrees))
 
         return data
-
-
-
-    def current_position(self):
-        """
-        Retreave the current position.
-        """
-        self.arcom.write_array([self.COM_GETCURRENTPOS])
-        data_in_bytes = b''.join(self.arcom.read_bytes_array(2))
-        ticks = int.from_bytes( data_in_bytes, byteorder='little', signed=True)
-        return self.__pos_2_degrees(ticks)
-
-    def set_zero_position(self):
-        """
-        Set current rotary encoder position to zero.
-        """
-        self.arcom.write_array([self.COM_SETZEROPOS])
 
     def set_prefix(self, prefix):
         """
@@ -159,7 +182,7 @@ class RotaryEncoderModule(object):
         Set 1-character prefix for module output stream.
         """
         self.arcom.write_array([self.COM_SETPREFIX, prefix])
-        return self.arcom.read_uint8()==1
+        return self.arcom.read_uint8() == 1
 
     def set_thresholds(self, thresholds):
         """
@@ -167,23 +190,10 @@ class RotaryEncoderModule(object):
 
         :ivar list(int) thresholds: List, in maximum, of 6 thresholds to trigger events.
         """
-        data  = ArduinoTypes.get_uint8_array([self.COM_SETTHRESHOLDS, len(thresholds) ])
-        data += ArduinoTypes.get_uint16_array([ self.__degrees_2_pos(thresh) for thresh in thresholds])
-        self.arcom.write_array(data )
-        return self.arcom.read_uint8()==1
-
-    def set_position(self, degrees):
-        """
-        Set the current position in degrees.
-
-        :ivar int degrees: current position in degrees.
-        """
-        ticks = self.__degrees_2_pos(degrees)
-        data  = ArduinoTypes.get_uint8_array([self.COM_SETPOS])
-        data += ticks.to_bytes(2, byteorder='little', signed=True)
-
+        data = ArduinoTypes.get_uint8_array([self.COM_SETTHRESHOLDS, len(thresholds)])
+        data += ArduinoTypes.get_uint16_array([self.__degrees_2_pos(thresh) for thresh in thresholds])
         self.arcom.write_array(data)
-        return self.arcom.read_uint8()==1
+        return self.arcom.read_uint8() == 1
 
     def set_wrappoint(self, wrap_point):
         """
@@ -192,24 +202,11 @@ class RotaryEncoderModule(object):
         :ivar int wrap_point: number of tics in a half-rotation.
         """
         ticks = self.__degrees_2_pos(wrap_point)
-        self.arcom.write_array([self.COM_SETWRAPPOINT]+ ArduinoTypes.get_uint16_array([ticks]) )
-        return self.arcom.read_uint8()==1
-
-    def enable_thresholds(self, thresholds):
-        """
-        Enable the thresholds.
-
-        :ivar list(boolean) thresholds: list of 6 booleans indicating which thresholds are active to trigger events.
-        """
-        if len(thresholds)!=8: raise Exception('Thresholds array has to be of length 8')
-        string = ''.join(map(lambda x: str(int(x)), thresholds))
-        bits = int(string, 2)
-        self.arcom.write_array([self.COM_ENABLETHRESHOLDS, bits])
+        self.arcom.write_array([self.COM_SETWRAPPOINT] + ArduinoTypes.get_uint16_array([ticks]))
+        return self.arcom.read_uint8() == 1
 
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     import time
 
     m = RotaryEncoderModule('/dev/ttyACM0')
